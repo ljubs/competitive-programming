f785841d7c5ad02e4b0844e393602d272abebbbc
ljubs <ljubs97@icloud.com>
2025-04-15 23:26:16 +0200

    add missing number solution

---
+++ cses/intro/missingNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll expectedSum = (n * (n + 1)) / 2; // Sum of numbers from 1 to n (triangular number)
+    ll actualSum = 0;
+    ll num;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> num;
+        actualSum += num;
+    }
+
+    ll missingNumber = expectedSum - actualSum;
+    cout << missingNumber << endl;
+    return 0;
+}
